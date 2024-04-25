27d25fbe7e48009fed3cd3ce100bf487935b00ee
SatishDev92 <kcsatish404@gmail.com>
2024-04-25 14:33:58 +0545

    js code

---
+++ add.js
@@ -0,0 +1,12 @@
+let con=document.querySelector("#container")
+let love=document.querySelector("i")
+con.addEventListener("dblclick", ()=>{
+    love.style.transform="translate(-50%,-50%) scale(1)";
+    love.style.opacity=0.8
+setTimeout(()=>{
+    love.style.opacity=0;
+},1000)
+setTimeout(()=>{
+    love.style.transform="translate(-50%,-50%) scale(0)"
+},2000)
+})
