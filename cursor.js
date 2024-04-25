a4e8a1f243da426545c1863dcf9751c6038cd7c7
SatishDev92 <kcsatish404@gmail.com>
2024-04-25 16:01:56 +0545

    changed cursor js

--- cursor.js
+++ cursor.js
@@ -1,8 +1,14 @@
-let main=document.querySelector("#main")
-let crsr=document.querySelector("#cursor")
+let elem=document.querySelectorAll(".elm")
+elem.forEach((val)=>{
 
-main.addEventListener("mousemove",(dets)=>{
-    crsr.style.left=dets.x+"px"
-    crsr.style.top=dets.y+"px"
-}
-)
+val.addEventListener("mouseenter", ()=>{
+val.childNodes[3].style.opacity=1;
+});
+val.addEventListener("mouseleave", ()=>{
+    val.childNodes[3].style.opacity=0;
+    });
+    val.addEventListener("mousemove", (dts)=>{
+        val.childNodes[3].style.left =dts.x +"px"
+        val.childNodes[3].style.top =dts.y +"px"
+    });
+    })
