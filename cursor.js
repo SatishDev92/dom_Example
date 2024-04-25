87d1c936bf28496203356316ef4cd0f1498facf2
SatishDev92 <kcsatish404@gmail.com>
2024-04-25 15:10:35 +0545

    js for cursor

---
+++ cursor.js
@@ -0,0 +1,8 @@
+let main=document.querySelector("#main")
+let crsr=document.querySelector("#cursor")
+
+main.addEventListener("mousemove",(dets)=>{
+    crsr.style.left=dets.x+"px"
+    crsr.style.top=dets.y+"px"
+}
+)
